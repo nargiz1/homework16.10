74cc4a5add312c175d2b2fb1e22a480fdf928bf4
nargiz1 <90691154+nargiz1@users.noreply.github.com>
2021-10-15 19:19:35 +0400

    homework 16.10

---
+++ Advertiser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework16._10
+{
+    class Advertiser: User
+    {
+        public Advertisement Advert { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Advert}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Homework16._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Advertisement ad1 = new Advertisement();
+            ad1.Name = "Hunger Games";
+            ad1.Price = 15;
+            ad1.SellerName = "Ali&Nino";
+            ad1.category = Category.Books;
+            
+            Advertisement ad2 = new Advertisement();
+            ad2.Name = "Hunger Games 2";
+            ad2.Price = 12;
+            ad2.SellerName = "Ali&Nino";
+            ad2.category = Category.Books;
+
+            Advertiser adv1 = new Advertiser();
+            adv1.UserName = "Kamil";
+            adv1.UserSurname = "Memmedov";
+            adv1.UserAge = 37;
+            adv1.Advert = ad1;
+
+            Advertisement[] arr = new Advertisement[2];
+            arr[0] = ad1;
+            arr[1] = ad2;
+            try
+            {
+                Array.Sort(arr);
+                foreach (Advertisement item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("wrong input");
+            }
+
+            Console.WriteLine(ad1>ad2);
+            Console.WriteLine(ad1.Sum(ad2));
+
+        }
+
+        
+    }
+}
